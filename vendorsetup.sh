5e309ef83647262c84e1477a44aedd53a13f85fd
lordeko <mattia.ruggeri88@gmail.com>
2015-05-07 10:17:50 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo aosip_shamu-user
 add_lunch_combo aosip_hammerhead-user
+add_lunch_combo aosip_hlte-user
 add_lunch_combo aosip_mako-userdebug
 add_lunch_combo aosip_grouper-userdebug
 add_lunch_combo aosip_manta-userdebug
